36413b0b85bff1d127b850bc49ba5f51198c6b89
Mario Lucassen <mario.lucassen@hotmail.com>
2018-10-25 21:24:09 +0200

    Removed old folder

--- Ultrix.WebUI/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ultrix.WebUI.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
