a92db6603b0ef6a7c6cfb6a4cffee672f4c74344
Mario Lucassen <mario.lucassen@hotmail.com>
2018-11-29 15:50:46 +0100

    Renamed CollectionPresenterViewModel to CollectionCarouselViewModel

--- Ultrix.Presentation/ViewModels/Collection_/CollectionCarouselViewModel.cs
+++ Ultrix.Presentation/ViewModels/Collection_/CollectionCarouselViewModel.cs
@@ -2,12 +2,12 @@
 
 namespace Ultrix.Presentation.ViewModels.Collection_
 {
-    public class CollectionPresenterViewModel
+    public class CollectionCarouselViewModel
     {
         public Collection Collection { get; private set; }
         public bool IsSubscribable { get; private set; }
 
-        public CollectionPresenterViewModel(Collection collection, bool isSubscribable)
+        public CollectionCarouselViewModel(Collection collection, bool isSubscribable)
         {
             Collection = collection;
             IsSubscribable = isSubscribable;
